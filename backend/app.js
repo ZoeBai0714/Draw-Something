31e3a5b44be1243f4cc9b0b994ea3c8d9bb024da
github username <github email address>
2019-05-01 19:47:12 -0500

    react frontend setup

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const pry = require('pryjs')
-const io = require('socket.io')()
+//const io = require('socket.io')(server)
 
 
 app.get('/', (req, res)=> res.send('Hello World'))
-
+//io.on('connection', (socket)=>console.log("New user connected"))
 app.listen(3000)
 console.log("you're listening to 3000...")
   
