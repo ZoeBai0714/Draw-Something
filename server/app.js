473a19d7b0a8f43ac53241f58dce434c6d661204
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-05-02 14:59:12 -0500

    done

--- server/app.js
+++ server/app.js
@@ -1,3 +1,14 @@
+const app = require('express')();
+
+app.listen(3000, () => {
+    console.log(`Listening on port 3000`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
 /*
   app.js is the entry point
   build models and controllers for backend
