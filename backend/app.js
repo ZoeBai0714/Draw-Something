f9b588f15a348a26d934bbd0e65f7e3437e26a83
github username <github email address>
2019-05-01 20:30:06 -0500

    framework setup

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
 const pry = require('pryjs')
-//const io = require('socket.io')(server)
+const http = require('http').Server(app)
+//const io = require('socket.io')(http)
 
 
-app.get('/', (req, res)=> res.send('Hello World'))
-//io.on('connection', (socket)=>console.log("New user connected"))
+app.get('/', (req, res)=> res.send('Hello World'))//'/frontend/index.html',{ root: "Draw-Something" 
 app.listen(3000)
 console.log("you're listening to 3000...")
+
+/* 
+ io.on('connection', function(socket){
+    console.log('an user connected');
+  });
+*/
+
   
+//io.listen(3000)
