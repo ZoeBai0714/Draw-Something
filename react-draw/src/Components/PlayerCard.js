c53d44deb66848b1781072b5c5360b4d35727707
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-05-02 20:51:51 -0500

    added Player component

--- react-draw/src/Components/PlayerCard.js
+++ react-draw/src/Components/PlayerCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Person from '../Components/Player'
 
 export default class PlayerCard extends React.Component {
 
@@ -7,6 +8,7 @@ export default class PlayerCard extends React.Component {
     render() {
         return (
             <div>
+                <Person/>
                 <img src={this.props.player.avatar}></img>
                 <h1>{this.props.player.name}</h1>
                 <p>Description: {this.props.player.description}</p>
