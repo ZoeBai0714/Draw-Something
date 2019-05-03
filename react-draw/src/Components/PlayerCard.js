95202e7ef14292c6df6cb4057fbf991af7419d38
github username <github email address>
2019-05-03 10:10:36 -0500

    ui cards

--- react-draw/src/Components/PlayerCard.js
+++ react-draw/src/Components/PlayerCard.js
@@ -6,10 +6,18 @@ export default class PlayerCard extends React.Component {
 
     render() {
         return (
-            <div>
-                <img src={this.props.player.avatar}></img>
-                <h1>{this.props.player.name}</h1>
-                <p>Description: {this.props.player.description}</p>
+            <div style={{float: "right", top: 100}}className="ui column">
+                <div className="ui card">
+                    <div className="image">
+                        <img src={this.props.player.avatar}></img>
+                    </div>
+                    <div className="content">
+                        <h3>{this.props.player.name}</h3>
+                        <div className="meta text-wrap">
+                            <p>Description: {this.props.player.description}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
