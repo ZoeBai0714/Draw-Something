454653448636cc17a9fcaf138cf61700ff9ec736
github username <github email address>
2019-05-08 12:29:48 -0500

    landing page is fully functioning and beautiful

--- react-draw/src/Containers/Home.js
+++ react-draw/src/Containers/Home.js
@@ -1,11 +1,69 @@
 import React from 'react'
-
+import '../Home.css'
+import {Link} from 'react-router-dom'
+ 
 
 
 export default class Home extends React.Component {
     render() {
       return(
-        <img class="thumbnail" src="https://i.pinimg.com/originals/e7/d7/19/e7d7190f0b5b3abd4f6c17e2c7989ec3.jpg" alt="image" style = {{"height": "900px"}}/>
+        <body>
+          <div class="first-pic">
+            <div class="ptext">
+              <span class="border">
+                <Link to = '/draw'>Draw!</Link>
+              </span>
+            </div>
+          </div>
+
+          <section class="section section-light">
+            <h2>About</h2>
+            <p className = "section-text">
+              This project is brought to you by Jack Rotta, Zoe Bai, and Robert Vidal. The goal in our Mod 4 project at Flatiron was to create a sketching 
+              app(Draw!) that is as real-time as possible where users could collaborate with their friends/coworkers in an easy way. We really pushed ourselves 
+              to finish this project in a little over a week, hope you enjoy it!
+            </p>
+          </section>
+
+          <div class="pimg2">
+            <div class="ptext">
+              <span class="border">
+                Collaboration
+              </span>
+            </div>
+          </div>
+
+          <section class="section section-dark">
+            
+            <p className = "section-text">
+                Work together with friends and coworkers to draw out your ideas for your next big pitch or website! <br /> We offer a variety of styles and brush widths
+                to hopefully accomplish any of your needs!
+            </p>
+          </section>
+
+          <div class="pimg3">
+            <div class="ptext">
+              <span class="border">
+              Doodle
+              </span>
+            </div>
+          </div>
+
+          <section class="section section-dark">
+            <p className = "section-text">
+                And if you would like to, use Draw! to doodle out your thoughts, feelings, or just randomly sketch your hours away! <br />
+                Or even use this as a drawing charades game!
+            </p>
+          </section>
+
+          <div class="first-pic">
+            <div class="ptext">
+            </div>
+          </div>
+        </body>
+
+
+
       )
     }
 }
