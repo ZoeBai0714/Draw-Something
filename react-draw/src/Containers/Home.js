0d39852ddc0d179a462fd480d58494f5cf585b1b
github username <github email address>
2019-05-08 08:39:59 -0500

    starting to make the home page

---
+++ react-draw/src/Containers/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default class Home extends React.Component {
+    render() {
+      return(
+        <img class="thumbnail" src="https://i.pinimg.com/originals/e7/d7/19/e7d7190f0b5b3abd4f6c17e2c7989ec3.jpg" alt="image" style = {{"height": "900px"}}/>
+      )
+    }
+}
